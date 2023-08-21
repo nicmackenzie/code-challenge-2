2f12eab839e915d74972d978b1c41c54dbaa6722
nicmackenzie <nicmackenzie@live.com>
2023-08-21 09:18:00 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+put 'Seeding data...'
+Hero.create([{name: 'Bruce Wayne',super_name: 'Batman'},{name: 'Matt Murdock',super_name: 'Daredevil'},{name: 'Tony Stark',super_name: 'Iron Man'}])
+Power.create([{name: 'Super Strength',description: 'gives the wielder super-human strengths'},
+              {name: 'Flight',description: 'gives the wielder the ability to fly through the skies at supersonic speed'},
+              {name: 'Super speed', description: 'gives the wielder the ability at supersonic speed'}])
+HeroPower.create([{strength: 'Average',hero_id: 1, power_id: 1},{strength: 'Strong',hero_id: 1, power_id: 2},{strength: 'Strong',hero_id: 2, power_id: 1}])              
+put 'Seeded data...'
