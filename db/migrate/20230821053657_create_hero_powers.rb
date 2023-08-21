e8087edc7e986bff402c017b02bb6c9c464214aa
nicmackenzie <nicmackenzie@live.com>
2023-08-21 08:38:12 +0300

    define table columns for hero_powers table

---
+++ db/migrate/20230821053657_create_hero_powers.rb
@@ -0,0 +1,10 @@
+class CreateHeroPowers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :hero_powers do |t|
+      t.string :strength
+      t.integer :hero_id
+      t.integer :power_id
+      t.timestamps
+    end
+  end
+end
